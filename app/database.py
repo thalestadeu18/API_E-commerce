854837aa89bcb271e854b4b6757aa0d2bea40427
thalestadeu18 <thalestadeu18@gmail.com>
2025-11-27 14:10:39 -0300

    Add files via upload

--- app/database.py
+++ app/database.py
@@ -1,9 +1,19 @@
+import oracledb
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+path_to_instant_client = r"D:\oracle\instantclient_23_0"
+
+try:
+    oracledb.init_oracle_client(lib_dir=path_to_instant_client)
+    
+except Exception as e:
+    print(f"Erro ao iniciar Oracle Client: {e}")
+    print("Verifique se o caminho da pasta está correto e se baixou a versão x64.")
+
 # SEUS DADOS
 DB_USER = "THALESTADEU18_SCHEMA_T8VNF"
-DB_PASSWORD = "<SUA_SENHA_AQUI>"  # <--- COLOCAR SUA SENHA AQUI
+DB_PASSWORD = "Z9D32n1IQNU#Q0FUECOPG8V9YQ34MN"  # <--- COLOCAR SUA SENHA AQUI
 DB_HOST = "db.freesql.com"
 DB_PORT = "1521"
 DB_SERVICE = "23ai_34ui2"
--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse  # <--- 1. Importe isto
 from controllers import usuario_controller
 from database import Base, engine
 
 # Cria as tabelas no banco automaticamente
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="API de Usuários - Projeto POO")
+
 # Inclui as rotas do controller
 app.include_router(usuario_controller.router)
+
+# --- ADICIONE ESTA PARTE NO FINAL ---
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
