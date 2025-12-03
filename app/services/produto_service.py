74768211367c995c55732ee8824eb0345b33e0d1
Wesleycodes <89798368+Wesleycodes@users.noreply.github.com>
2025-12-03 17:20:12 -0300

    Implement product update function
    Added a function to update a product from the database.

--- app/services/produto_service.py
+++ app/services/produto_service.py
@@ -31,4 +31,24 @@ def deletar_produto(db, produto_id: int):
     db.delete(produto)
     db.commit()
     return True
+    
+# Wesley: Função para atualizar o produto 
+def atualizar_produto(db: Session, produto_id: int, nome: str = None, valor: float = None, descricao: str = None):
+    # Busca o produto
+    produto = db.query(Produto).filter(Produto.id == produto_id).first()
+    
+    if not produto:
+        return None # Ou apresentar erro
 
+    # Atualiza somente os campos que foram enviados
+    if nome:
+        produto.nome = nome
+    if valor:
+        produto.valor = valor
+    if descricao:
+        produto.descricao = descricao
+
+    # Salva as modificações
+    db.commit()
+    db.refresh(produto)
+    return produto
