c7d1eea53f56a35ed511946a56eddcc95e766341
Daniel Duarte Lopes <danieldev871@gmail.com>
2025-12-03 14:49:33 -0300

    Modify product routes for creation and deletion
    Updated the product creation route to accept parameters directly in the URL and added a delete route for products.

--- app/controllers/produto_controller.py
+++ app/controllers/produto_controller.py
@@ -5,7 +5,7 @@ from services import produto_service
 
 router = APIRouter(prefix="/produtos", tags=["Catálogo de E-commerce"])
 
-@router.post("/")
+@router.post("/{nome}/{valor}/{quantidade}")#Daniel:alterei a rota para conseguimos adcionar produtos pelo gitbash
 def criar_produto(
     nome: str, 
     valor: float, 
@@ -21,4 +21,12 @@ def criar_produto(
 
 @router.get("/")
 def listar_produtos(db: Session = Depends(get_db)):
+
     return produto_service.listar_produtos(db)
+#Daniel:isso e para deletar pelo gitbash rota:curl -X DELETE http://127.0.0.1:8000/produtos/{poduto_id}
+@router.delete("/{produto_id}")
+def deletar_produto(produto_id: int, db: Session = Depends(get_db)):
+    sucesso = produto_service.deletar_produto(db, produto_id)
+    if not sucesso:
+        raise HTTPException(status_code=404, detail="Produto não encontrado")
+    return {"detail": "Produto deletado com sucesso"}
